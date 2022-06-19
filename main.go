5e7747eb25cf44ba8d3fae0c77e27b06549213f8
Quyen Tat <tvq9612@gmail.com>
2022-06-19 08:32:59 +0700

    add core function

--- main.go
+++ main.go
@@ -1,7 +1,103 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type quote struct {
+	Text   string `json:"q"`
+	Author string `json:"a"`
+}
+
+type slackDataSend struct {
+	Text string `json:"text"`
+}
 
 func main() {
-	fmt.Println("Hello World")
+	jsonQuote := getQuote()
+	log.Println(jsonQuote)
+
+	// Slack
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	slackUrl := os.Getenv("SLACK_WEBHOOK_URL")
+	dataSend := slackDataSend{
+		Text: fmt.Sprintf("%s - %s", jsonQuote.Text, jsonQuote.Author),
+	}
+
+	jsonDataSend, jsonErr := json.Marshal(dataSend)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	byteDataSend := bytes.NewBuffer(jsonDataSend)
+
+	req, resErr := http.NewRequest(http.MethodPost, slackUrl, byteDataSend)
+	if resErr != nil {
+		log.Fatal(resErr)
+	}
+
+	httpClient := http.Client{
+		Timeout: time.Second * 2,
+	}
+
+	res, resErr := httpClient.Do(req)
+	if resErr != nil {
+		log.Fatal(resErr)
+	}
+
+	var res1 map[string]interface{}
+
+	json.NewDecoder(res.Body).Decode(&res1)
+
+	fmt.Println(res)
+
+	//log.Println(res)
+
+}
+
+func getQuote() quote {
+	quoteUrl := "https://zenquotes.io/api/today"
+
+	httpClient := http.Client{
+		Timeout: time.Second * 2,
+	}
+
+	req, reqErr := http.NewRequest(http.MethodGet, quoteUrl, nil)
+	if reqErr != nil {
+		log.Fatal(reqErr)
+	}
+
+	res, resErr := httpClient.Do(req)
+	if resErr != nil {
+		log.Fatal(resErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	var quotes []quote
+	jsonErr := json.Unmarshal(body, &quotes)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return quotes[0]
 }
